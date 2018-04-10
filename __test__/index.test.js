0236c49d1ab85c34333a4f6163b0c5e3bf423408
phillyo2 <mail.phillyo2@gmail.com>
2018-04-10 17:30:35 -0400

    added funtionality

---
+++ __test__/index.test.js
@@ -0,0 +1,12 @@
+import modicopy from '../index'
+
+describe('modicopy', ()=>{
+    describe('$push', ()=>{
+        it('should push to an array and not mutate th original', ()=>{
+            const original = {a: [1]}
+            const updatedCopy = modicopy(original).a.$push(2)
+            expect(original).toEqual({a: [1]})
+            expect(updatedCopy).toEqual({a: [1, 2]})
+        })
+    })
+})
