c2f495e81912de5adaebfcfd1cbf57d320fd70bf
Phillip Ward <mail.phillyo2@gmail.com>
2023-09-25 15:15:56 -0400

    add copy function and update readme. STILL NEED MORE TESTS

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -5,8 +5,18 @@ describe('modicopy', ()=>{
         it('should push to an array and not mutate th original', ()=>{
             const original = {a: [1]}
             const updatedCopy = modicopy(original).a.$push(2)
-            expect(original).toEqual({a: [1]})
+            expect(original).toBe(original)
             expect(updatedCopy).toEqual({a: [1, 2]})
+        })
+    })
+
+    describe('$copy', ()=>{
+        it('should return a copy (have a different reference)', ()=>{
+            const original = {a: [1]}
+            const copy = modicopy(original).$copy()
+            expect(original).toBe(original)
+            expect(copy).not.toBe(original)
+            expect(copy).toEqual(original)
         })
     })
 })
